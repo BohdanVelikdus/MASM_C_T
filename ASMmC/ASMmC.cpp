3b3a6915a452266026a047d4f92d7f4923e5b26a
Bohdan Velikdus <140083@student.uni.opole.pl>
2024-03-11 17:11:58 +0100

    Function calling and using stack

--- ASMmC/ASMmC.cpp
+++ ASMmC/ASMmC.cpp
@@ -2,24 +2,15 @@
 #include "pch.h"
 #include <stdlib.h>
 
-extern "C" int IntegerMulDiv(int a, int b, int* quo, int* res, int* rem);
+extern "C" int CalculateSum(int a, int b, int c, int* s1, int* s2, int* s3);
 
 int main()
 {
-	int a = 12;
-	int b = 5;
-	int* quo = new int();
-	int* res = new int();
-	int* rem = new int();
+	int a = 2, b = 3, c = 4;
+	int s1, s2, s3;
 
-	IntegerMulDiv(a, b, quo, res, rem);
-
-	std::cout << "quo: " << *quo << std::endl;
-	std::cout << "res: " << *res << std::endl;
-	std::cout << "rem: " << *rem << std::endl;
-
-	delete quo;
-	delete res;
-	delete rem;
+	CalculateSum(a, b, c, &s1, &s2, &s3);
+	std::cout << a << " " << b << " " << c << "\n";
+	std::cout << s1 << " " << s2 << " " << s3 << "\n";
 }
 
